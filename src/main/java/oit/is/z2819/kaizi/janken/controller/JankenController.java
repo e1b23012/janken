c54c3955eacd172e6340c0bc6693e0a59a5f3b54
e1b23012 <e1b23012@oit.ac.jp>
2025-10-20 13:12:49 +0900

    Dependency Injectionを用いたオブジェクトの共有

--- src/main/java/oit/is/z2819/kaizi/janken/controller/JankenController.java
+++ src/main/java/oit/is/z2819/kaizi/janken/controller/JankenController.java
@@ -2,24 +2,26 @@ package oit.is.z2819.kaizi.janken.controller;
 
 import java.security.Principal;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import oit.is.z2819.kaizi.janken.model.Entry;
+
 @Controller
 public class JankenController {
 
-  // @PostMapping("/janken")
-  // public String janken(@RequestParam String username, ModelMap model) {
-  // model.addAttribute("username", username);
-  // return "janken.html";
-  // }
+  @Autowired
+  private Entry entry;
 
   @GetMapping("/janken")
   public String jankenHand(@RequestParam(required = false) String hand, ModelMap model, Principal prin) {
     model.addAttribute("username", prin.getName());
+    entry.addUser(prin.getName());
+    model.addAttribute("room", entry);
     if (hand == null) {
       return "janken.html";
     }
