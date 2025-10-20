b0a18ce164144978bf057301aae342fc77d9e3a8
e1b23012 <e1b23012@oit.ac.jp>
2025-10-20 12:54:10 +0900

    JankenAuthConfiguration.javaを作成

--- src/main/java/oit/is/z2819/kaizi/janken/controller/JankenController.java
+++ src/main/java/oit/is/z2819/kaizi/janken/controller/JankenController.java
@@ -1,5 +1,7 @@
 package oit.is.z2819.kaizi.janken.controller;
 
+import java.security.Principal;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,48 +11,47 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class JankenController {
 
-  @PostMapping("/janken")
-  public String janken(@RequestParam String username, ModelMap model) {
-  model.addAttribute("username", username);
-  return "janken.html";
-  }
+  // @PostMapping("/janken")
+  // public String janken(@RequestParam String username, ModelMap model) {
+  // model.addAttribute("username", username);
+  // return "janken.html";
+  // }
 
   @GetMapping("/janken")
-  public String jankenGet(@RequestParam String hand, ModelMap model) {
-  model.addAttribute("playerHand", hand);
-  String cpuHand = "gu";
-  model.addAttribute("cpuHand", cpuHand);
+  public String jankenHand(@RequestParam(required = false) String hand, ModelMap model, Principal prin) {
+    model.addAttribute("username", prin.getName());
+    if (hand == null) {
+      return "janken.html";
+    }
+    model.addAttribute("playerHand", hand);
+    String cpuHand = "gu";
+    model.addAttribute("cpuHand", cpuHand);
 
-  String result = "";
-  if (hand.equals(cpuHand)) {
-  result = "引き分け";
-  } else if (hand.equals("gu")) {
-  if (cpuHand.equals("choki")) {
-  result = "勝ち";
-  } else {
-  result = "負け";
-  }
+    String result = "";
+    if (hand.equals(cpuHand)) {
+      result = "引き分け";
+    } else if (hand.equals("gu")) {
+      if (cpuHand.equals("choki")) {
+        result = "勝ち";
+      } else {
+        result = "負け";
+      }
 
-  } else if (hand.equals("choki")) {
-  if (cpuHand.equals("pa")) {
-  result = "勝ち";
-  } else {
-  result = "負け";
-  }
-  } else if (hand.equals("pa")) {
-  if (cpuHand.equals("gu")) {
-  result = "勝ち";
-  } else {
-  result = "負け";
-  }
-  }
-  model.addAttribute("result", result);
+    } else if (hand.equals("choki")) {
+      if (cpuHand.equals("pa")) {
+        result = "勝ち";
+      } else {
+        result = "負け";
+      }
+    } else if (hand.equals("pa")) {
+      if (cpuHand.equals("gu")) {
+        result = "勝ち";
+      } else {
+        result = "負け";
+      }
+    }
+    model.addAttribute("result", result);
 
-  return "janken.html";
-  }
-
-  @GetMapping("/janken")
-  public String jankenGet2(ModelMap model) {
     return "janken.html";
   }
 
