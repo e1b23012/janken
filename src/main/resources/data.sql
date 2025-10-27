c613b191145733a24b74e26566efe872157509eb
e1b23012 <e1b23012@oit.ac.jp>
2025-10-27 14:03:22 +0900

    DBの初期化処理の実装

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (name) VALUES ('CPU');
+INSERT INTO users (name) VALUES ('ほんだ');
+INSERT INTO matches (user1, user2, user1Hand, user2Hand) VALUES (2, 1, 'グー', 'チョキ');
+INSERT INTO matches (user1, user2, user1Hand, user2Hand) VALUES (2, 1, 'グー', 'グー');
+INSERT INTO matches (user1, user2, user1Hand, user2Hand) VALUES (2, 1, 'グー', 'パー');
